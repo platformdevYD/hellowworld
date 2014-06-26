939ae788c7805522ca7c7faa42e9490abc43a3e9
geniusjh <geniusjh@ydonline.co.kr>
2014-06-26 12:18:17 +0900

    file add

---
+++ hello-world-server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type':'text/plain'});
+  res.end('Hello world!');
+});
+server.listen(80);
+console.log('Server is started on port 80');
