1d3714a86a58d41505b6afc47667a658e5423cc7
geniusjh <geniusjh@ydonline.co.kr>
2014-07-14 19:45:14 +0900

    local edit test

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('Hello world!');
+  res.end('Hello world! local pc edit!!!');
 });
 
 server.listen(8090);
