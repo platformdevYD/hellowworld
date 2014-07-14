602c68a3014a804fb4e01580288c19b45de556fb
platformdevYD <team_platformdev@ydonline.co.kr>
2014-07-14 20:46:39 +0900

    e

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('Hello world!');
+  res.end('Hello world! restart test!');
 });
 
 server.listen(8090);
