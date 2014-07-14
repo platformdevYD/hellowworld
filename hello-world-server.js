4c96de1d0722ce5fac71182ec5e121151f9ad08d
platformdevYD <team_platformdev@ydonline.co.kr>
2014-07-14 17:08:46 +0900

    update port

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,8 +1,8 @@
 var http = require('http');
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('Hello world!!');
+  res.end('Hello world!');
 });
 
-server.listen(80);
+server.listen(8090);
 console.log('Server is started on port 80');
