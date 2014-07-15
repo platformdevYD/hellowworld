d752b8a688550100f179ceeb1a66b562c6385667
geniusjh <geniusjh@ydonline.co.kr>
2014-07-15 18:16:06 +0900

    test

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('Hello world! restart test!!');
+  res.end('Hello world!');
 });
 
 server.listen(8090);
